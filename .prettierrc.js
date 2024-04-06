2f30aed58cc82056a09103f2e801bf240c134f2a
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-06 20:06:39 +0900

    reset 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,7 @@ module.exports = {
     bracketSpacing: false,
     jsxBracketSameLine: true,
     singleQuote: true,
+    doubleQuote: false,
     trailingComma: 'none',
     arrowParens: 'avoid',
     semi: false,
