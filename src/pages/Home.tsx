a2377bcce1860f7463faa4dcd71ac513f8ed402a
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-07 14:44:02 +0900

    feat: HomeTitle 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import BottomNav from '../components/common/BottomNav'
+import HomeTitle from '../components/home/HomeTitle'
 import '../css/style.css'
 export default function Home() {
   return (
     <>
+      <HomeTitle userName="황혜경" amount={30000} tag="아가리 저축러에서 벗어나자" />
       <BottomNav currentMenu="home" />
     </>
   )
