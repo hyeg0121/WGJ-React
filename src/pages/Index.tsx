af0f26269ec8284c7e5bb328480fb0535835b88b
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-08 18:30:58 +0900

    fix: Reset 제거

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import { Reset } from 'styled-reset';
 import MainButton from '../components/forms/MainButton';
 import styles from '../css/page/Index.module.css';
 import '../css/style.css';
@@ -12,7 +11,6 @@ export default function Index() {
     const goToSignUp = () => navigation('/signup');
     return (
         <div className='main_container'>
-            <Reset />
             <div className={styles.title_container}>
                 <h2 className={styles.sub_title}>우리는 거지가 지겹다</h2>
                 <h1 className={styles.main_title}>우거지</h1>
