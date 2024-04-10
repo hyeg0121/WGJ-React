74b9965264cb1c2a3577cf1771e72aaa6e6f0655
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-10 17:22:25 +0900

    feat: Provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter } from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom'
 import RouterSetup from './routes/RouteSetup'
+import store from './store'
 
 export default function App() {
   return (
-    <BrowserRouter> 
-      <RouterSetup />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <RouterSetup />
+      </BrowserRouter>
+    </Provider>
   )
 }
